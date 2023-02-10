a59ecd1592949144da7cdaffd8023677ef634677
Beatriz Moura <b3am0ur4@outlook.com>
2023-02-10 07:44:14 -0300

    Comando de soma, convertendo string p/ int

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-name = input ('Qual é o seu nome?');
-dia = input ('Qual o dia em que você nasceu?');
-mes = input ('Qual o mês em que você nasceu?');
-ano = input ('Qual o ano em que você nasceu?')
-mensagem = 'Olá, ' + name + ', prazer em te conhecer!'
-dataNascimento = dia + mes + ano;
-print (mensagem);
-print (dataNascimento);
-
+name = input('Qual é o seu nome?')
+dia = input('Qual o dia em que você nasceu?')
+mes = input('Qual o mês em que você nasceu?')
+ano = input('Qual o ano em que você nasceu?')
+mensagem = 'Olá, {}'.format(name) + ', prazer em te conhecer!'
+dataNascimento = 'Data de nascimento: ' + dia + '/' + mes + '/' + ano
+print(mensagem)
+print(dataNascimento)
